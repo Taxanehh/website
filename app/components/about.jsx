dbd5578436abbf648367396df69c0a53209c5096
Paul Stokreef <paul.stokreef@student.hva.nl>
2024-09-29 17:45:50 +0200

    About & Contact update

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -81,7 +81,7 @@ export default function About() {
               >
                 <h2 className="text-2xl font-semibold mb-2">My Journey</h2>
                 <p className="text-gray-400 mb-4">
-                Hello! I'm Paul, a cybersecurity enthusiast currently diving into my studies with a passion for tech and problem-solving. I have a background in Artificial Intelligence from the University of Amsterdam and hands-on experience in both front-end and back-end development, next to also having participated in many CTF's. I enjoy working with various tools and technologies and am always eager to learn something new. My journey has been shaped by a mix of academic learning and practical experimentation, and I’m excited to apply this blend of skills to the cybersecurity field. I’m also a fan of the collaborative spirit of the tech community, and I’m always open to contributing and learning from others. Speaking about open, <a className="text-gray-200 underline" href="https://github.com/Taxanehh/website-master" target="_blank" rel="noopener noreferrer">this website is open-source!</a> :)
+                Hello! I'm Paul, a cybersecurity enthusiast currently diving into my studies with a passion for tech and problem-solving. I have a background in Artificial Intelligence from the University of Amsterdam and hands-on experience in both front-end and back-end development, next to also having participated in many CTF's. I enjoy working with various tools and technologies and am always eager to learn something new. My journey has been shaped by a mix of academic learning and practical experimentation, and I’m excited to apply this blend of skills to the cybersecurity field. I’m also a fan of the collaborative spirit of the tech community, and I’m always open to contributing and learning from others. Speaking about open, <a className="text-gray-200 underline" href="https://github.com/Taxanehh/website" target="_blank" rel="noopener noreferrer">this website is open-source!</a> :)
                 </p>
               </motion.div>
 
@@ -89,12 +89,54 @@ export default function About() {
                 initial={{ opacity: 0, y: 30 }}
                 animate={{ opacity: 1, y: 0, transition: { duration: 1, delay: 1.5 } }}
                 className="p-8 bg-[#1a1a1a] rounded-lg shadow-md"
-              >
-                <h2 className="text-2xl font-semibold mb-2">Key Skills</h2>
+                >
+                <h2 className="text-2xl font-semibold mb-2">Skills</h2>
                 <p className="text-gray-400 mb-4">
-                  Secure coding, penetration testing, vulnerability management, and cloud infrastructure security.
+                    I can use the following languages:
                 </p>
+                <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mb-4">
+                    <img alt="Python" src="https://img.shields.io/badge/-Python-18181b?style=for-the-badge&logo=python&logoColor=3f3ff4" />
+                    <img alt="SQL" src="https://img.shields.io/badge/-SQL-18181b?style=for-the-badge&logo=postgresql&logoColor=3f3ff4" />
+                    <img alt="JavaScript" src="https://img.shields.io/badge/-JavaScript-18181b?style=for-the-badge&logo=javascript&logoColor=3f3ff4" />
+                    <img alt="TypeScript" src="https://img.shields.io/badge/-TypeScript-18181b?style=for-the-badge&logo=typescript&logoColor=3f3ff4" />
+                    <img alt="HTML5" src="https://img.shields.io/badge/-HTML5-18181b?style=for-the-badge&logo=html5&logoColor=3f3ff4" />
+                    <img alt="CSS3" src="https://img.shields.io/badge/-CSS3-18181b?style=for-the-badge&logo=css3&logoColor=3f3ff4" />
+                    <img alt="php" src="https://img.shields.io/badge/-PhP-18181b?style=for-the-badge&logo=php&logoColor=3c3cf0" />
+                </div>
+
+                <p className="text-gray-400 mb-4">
+                    I can use the following frameworks and libraries:
+                </p>
+                <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mb-4">
+                    <img alt="Flask" src="https://img.shields.io/badge/-Flask-18181b?style=for-the-badge&logo=flask&logoColor=3f3ff4" />
+                </div>
+
+                <p className="text-gray-400 mb-4">
+                    Miscellaneous tools that I use:
+                </p>
+                <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mb-4">
+                    <img alt="Linux" src="https://img.shields.io/badge/-Linux-18181b?style=for-the-badge&logo=linux&logoColor=3f3ff4" />
+                    <img alt="Git" src="https://img.shields.io/badge/-Git-18181b?style=for-the-badge&logo=git&logoColor=3f3ff4" />
+                    <img alt="NGINX" src="https://img.shields.io/badge/-NGINX-18181b?style=for-the-badge&logo=nginx&logoColor=3f3ff4" />
+                    <img alt="Metasploit" src="https://img.shields.io/badge/-Metasploit-18181b?style=for-the-badge&logo=metasploit&logoColor=3f3ff4" />
+                    <img alt="Burpsuite" src="https://img.shields.io/badge/-Burpsuite-18181b?style=for-the-badge&logo=burpsuite&logoColor=3f3ff4" />
+                    <img alt="Figma" src="https://img.shields.io/badge/-Figma-18181b?style=for-the-badge&logo=figma&logoColor=3c3cf0" />
+                </div>
+
+                <p className="text-gray-400 mb-4">
+                    Currently learning:
+                </p>
+                <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 mb-8">
+                    <img alt="React" src="https://img.shields.io/badge/-React-18181b?style=for-the-badge&logo=react&logoColor=3f3ff4" />
+                    <img alt="Next.js" src="https://img.shields.io/badge/-Next.js-18181b?style=for-the-badge&logo=next.js&logoColor=3f3ff4" />
+                    <img alt="Tailwind CSS" src="https://img.shields.io/badge/-Tailwind%20CSS-18181b?style=for-the-badge&logo=tailwindcss&logoColor=3f3ff4" />
+                </div>
+                
+                <div className="text-5xl font-bold mb-8 grid grid-rows-[250px_2fr_20px] items-center justify-items-center">
+                    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=taxanehh&count_private=true&show_icons=true&title_color=3c3cf0&text_color=ffffff&icon_color=F43F5E&bg_color=18181b"/>
+                </div>
               </motion.div>
+
             </section>
           </main>
         </motion.div>
--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
     <AnimatePresence>
       {!isLeaving && (
         <motion.div
-          className="min-h-full bg-[#101010] text-white px-8 sm:px-12 lg:px-24 xl:px-32 py-8"
+          className="min-h- bg-[#101010] text-white px-8 sm:px-12 lg:px-24 xl:px-32 py-8"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 1 } }}
           exit={{ opacity: 0, transition: { duration: 1 } }}
@@ -67,7 +67,7 @@ export default function Contact() {
 
             {/* Icons and Links */}
             <motion.div
-              className="flex space-x-8 text-4xl"
+              className="flex space-x-8 text-4xl flex-grow flex row-start-2 items-center sm:items-start relative z-10"
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0, transition: { duration: 1, delay: 0.9 } }}
             >
